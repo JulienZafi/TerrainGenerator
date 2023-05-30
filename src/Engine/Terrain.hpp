9b1c160c3ea78931807224117979202ae57d7f75
Julien ZAFIFOMENDRAHA <95655108+JulienZafi@users.noreply.github.com>
2023-05-30 16:44:38 +0200

    Add files via upload

--- src/Engine/Terrain.hpp
+++ src/Engine/Terrain.hpp
@@ -7,16 +7,17 @@
 #include <memory>
 #include <vector>
 
-#define NUM_CHUNCKS_TO_DISPLAY	3
+#define NUM_CHUNCKS_TO_DISPLAY	2
+#define CHUNK_WIDTH				50
+#define CHUNK_HEIGHT			50
 
 namespace Engine
 {	
 	class Terrain
 	{
 	public:
-		Terrain() = default;
+		Terrain() noexcept;
 		~Terrain() = default;
-		Terrain(unsigned int width, unsigned int height) noexcept;
 
 		void GenerateChunks() noexcept;
 		void UpdateChunks() noexcept;
@@ -25,11 +26,12 @@ namespace Engine
 		const float GetHeightAtPos(float const& x, float const& z) const noexcept;
 		void RenderChunks(Shader const& terrainShader) const noexcept;
 
+		[[nodiscard]] inline const unsigned int Width(){ return (NUM_CHUNCKS_TO_DISPLAY * CHUNK_WIDTH); }
+		[[nodiscard]] inline const unsigned int Height(){ return (NUM_CHUNCKS_TO_DISPLAY * CHUNK_HEIGHT); }
+
 	private:
-		unsigned int m_width;
-		unsigned int m_height;
 		float m_playerX;
 		float m_playerZ;
-		std::map <std::pair <float, float>, std::unique_ptr <Chunk>> m_chunks;
+		std::map <std::pair <int, int>, std::unique_ptr <Chunk>> m_chunks;
 	};
 }
